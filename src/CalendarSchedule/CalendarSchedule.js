19087e60bf71a239adefa37b53c84af7d0362ec4
Adam Main <northforth@me.com>
2021-02-10 18:21:35 -0700

    Added: user calendar schedule data

--- src/CalendarSchedule/CalendarSchedule.js
+++ src/CalendarSchedule/CalendarSchedule.js
@@ -1,12 +1,106 @@
 import React from 'react'
+import config from '../config'
 
 import './CalendarSchedule.css'
 
-function CalendarSchedule() {
+const getCalDay = day => {
+  switch(day) {
+    case 0:
+      return 'SU'
+    case 1:
+      return 'MO'
+    case 2:
+      return 'TU'
+    case 3:
+      return 'WE'
+    case 4:
+      return 'TH'
+    case 5:
+      return 'FR'
+    case 6:
+      return 'SA'
+    default:
+      return ''
+  }
+}
+
+const CalendarSchedule = (props) => {
+  // @TODO allow user to select calendar month 
+  const selectedMonth = '2'
+  // @TODO get logged in user's employee number (used for login) or get from FLICA after logged in??
+  const userId = '30984'
+  let [calendarDaysResponse, setCalendarDaysResponseData] = React.useState('')
+  let [userScheduleResponse, setUserScheduleResponseData] = React.useState('')
+  const fetchUserSchedule = React.useCallback(() => {
+    const fetchBody = {
+      method: 'GET',
+      headers: {
+        'content-type': 'application/json',
+        'Authorization': `Bearer ${config.API_KEY}`
+      }
+    }
+    Promise.all([
+      fetch(`${config.API_BASE_URL}/schedules/${selectedMonth}`, fetchBody),
+      fetch(`${config.API_BASE_URL}/schedules?id=${userId}`, fetchBody)
+    ])
+      .then(([calDaysRes, userScheduleRes]) => {
+        if (!calDaysRes.ok) {
+          return calDaysRes.json().then(e => Promise.reject(e))
+        }
+        if (!userScheduleRes.ok) {
+          return userScheduleRes.json().then(e => Promise.reject(e))
+        }
+        return Promise.all([calDaysRes.json(), userScheduleRes.json()])
+      })
+      .then(([calDays, userSchedule]) => {
+        setCalendarDaysResponseData(calDays)
+        setUserScheduleResponseData(userSchedule)
+      })
+      .catch(err => console.log('there has been an error.', err))
+  }, [])
+    
+  React.useEffect(() => {
+    fetchUserSchedule()
+  }, [fetchUserSchedule])
+
+  const renderTableRow = () => {
+    let calRows = 
+      calendarDaysResponse && calendarDaysResponse.days_in_month
+        .map(day => {
+          const calDate = new Date(day).getDate()
+          
+          const tripToday = userScheduleResponse ? userScheduleResponse.find(pairing => {
+            const startDate = new Date(pairing.pair_start).getDate()
+            return startDate === calDate
+          }) : {}
+
+          const lastLeg = props.pairingLegs ? props.pairingLegs.find(leg => {
+            const legArrivalDate = new Date(leg.arrival_time).getDate()
+            return legArrivalDate === calDate
+          }) : {}
+          
+          return (
+            <tr key={day}>
+              <td>{getCalDay(new Date(day).getDay())}</td>
+              <td>{calDate ? (calDate) : null}</td>
+              <td>{tripToday ? (tripToday.pairing_i) : null}</td>
+              <td>{lastLeg ? (lastLeg.arrival_service) : null}</td>
+            </tr>
+          )
+        })
+        console.log({calRows})
+    return calRows
+  }
+  const row = renderTableRow()
   return (
     <div className='CalendarSchedule'>
       <div className='CalendarSchedule__wrapper'>
-        Calendar Schedule
+        <h3>February Schedule</h3>
+        <table>
+          <tbody>
+            {row.length > 0 ? row : null}
+          </tbody>
+        </table>
       </div>
     </div>
   )
--- src/Dashboard/Dashboard.js
+++ src/Dashboard/Dashboard.js
@@ -2,20 +2,81 @@ import React from 'react'
 import CalendarSchedule from '../CalendarSchedule/CalendarSchedule'
 import OTFeed from '../OTFeed/OTFeed'
 import NotificationFilter from '../NotificationFilter/NotificationFilter'
+import config from '../config'
 import './Dashboard.css'
 
 function Dashboard() {
+  let [pairingsResponse, setPairingsResponseData] = React.useState('')
+  let [pairingLegsResponse, setPairingsLegsResponseData] = React.useState('')
+  let [employeeesResponse, setEmployeeesResData] = React.useState('')
+  let [layoversResponse, setLayoversResData] = React.useState('')
+  let [hotelsResponse, setHotelsResData] = React.useState('')
+
+  const fetchData = React.useCallback(() => { 
+    const fetchBody = {
+      method: 'GET',
+      headers: {
+        'content-type': 'application/json',
+        'Authorization': `Bearer ${config.API_KEY}`
+      }
+    }
+    Promise.all([
+      fetch(`${config.API_BASE_URL}/pairings/`, fetchBody),
+      fetch(`${config.API_BASE_URL}/pairing-legs/`, fetchBody),
+      fetch(`${config.API_BASE_URL}/employees/`, fetchBody),
+      fetch(`${config.API_BASE_URL}/layovers/`, fetchBody),
+      fetch(`${config.API_BASE_URL}/hotels/`, fetchBody)
+    ])
+      .then(([pairingsRes, pairingLegsRes, employeeesRes, layoversRes, hotelsRes]) => {
+        if (!pairingsRes.ok) {
+          return pairingsRes.json().then(e => Promise.reject(e))
+        }
+        if (!pairingLegsRes.ok) {
+          return pairingLegsRes.json().then(e => Promise.reject(e))
+        } 
+        if (!employeeesRes.ok) {
+          return employeeesRes.json().then(e => Promise.reject(e))
+        }
+        if (!layoversRes.ok) {
+          return layoversRes.json().then(e => Promise.reject(e))
+        } 
+        if (!hotelsRes.ok) {
+          return hotelsRes.json().then(e => Promise.reject(e))
+        } 
+        return Promise.all([pairingsRes.json(), pairingLegsRes.json(), employeeesRes.json(), layoversRes.json(), hotelsRes.json()])
+      })
+      .then(([pairings, pairingLegs, employeees, layovers, hotels]) => {
+        // @TODO setResonseData hook
+        setPairingsResponseData(pairings)
+        setPairingsLegsResponseData(pairingLegs.reverse())
+        setEmployeeesResData(employeees)
+        setLayoversResData(layovers)
+        setHotelsResData(hotels)
+      })
+      .catch(err => console.log('there has been an error.', err))
+  }, [])
+  
+  React.useEffect(() => {
+    fetchData()
+  }, [fetchData])
+
   return (
     <main className='Dashboard'>
       <div className='Dashboard__container'>
-        <aside className='Dashboard__aside--aside'>
-          <CalendarSchedule />
+        <aside className='Dashboard__aside'>
+          <CalendarSchedule 
+            pairings={pairingsResponse} 
+            pairingLegs={pairingLegsResponse}
+            employees={employeeesResponse}
+            layovers={layoversResponse}
+            hotels={hotelsResponse}
+          />
         </aside>
         <section className='Dashboard__section'>
           <h2>OPENTIME Feed</h2>
           <OTFeed />
         </section>
-        <aside className='Dashboard__aside--aside hidden'>
+        <aside className='Dashboard__aside hidden'>
           <NotificationFilter />
         </aside>
       </div>
