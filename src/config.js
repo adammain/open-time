358f12398cfe5af9e53193f4cd8375b64a561486
Adam Main <northforth@me.com>
2021-02-10 18:31:35 -0700

    Fixed: api url variable

--- src/config.js
+++ src/config.js
@@ -1,6 +1,5 @@
 module.exports = {
   PORT: process.env.PORT || 8080,
   API_KEY: process.env.REACT_APP_API_KEY,
-  API_BASE_URL: "http://localhost:8000/api"
-  // API_BASE_URL: process.env.REACT_APP_API_BASE_URL || "http://localhost:8000/api"
+  API_BASE_URL: process.env.REACT_APP_API_BASE_URL || "http://localhost:8000/api"
 }
